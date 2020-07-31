839334ab1425f32719c84076b42ab30edd7835cd
Harmeet Singh <58842145+Harmeet-singh-Khalsa@users.noreply.github.com>
2020-07-31 21:47:44 +0530

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -36,6 +36,34 @@ var firebaseConfig =
     }
   });
 
+
+  $("#btn-resetPassword").click(function()
+  {
+    var auth = firebase.auth();
+    var email = $("#exampleInputEmail").val();
+    if(email != "")
+    {
+      auth.sendPasswordResetEmail(email).then(function()
+      {
+        window.alert("Email has been sent to you please check."); 
+      }).catch(function(error)
+      {
+        var errorCode=error.code;
+        var errorMessage=error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        window.alert("Message: "+ errorMessage);
+
+      });
+    }
+    else{
+      window.alert("Please write your Email.");
+    }
+
+  });
+
+
+ 
   $("#btn-logout").click(function()
   {
     firebase.auth().signOut();
